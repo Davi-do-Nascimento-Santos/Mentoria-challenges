c897692c8c6b01bf5ff5557338c1141978f1813f
Davi Nascimento <davinascimento19072001@gmail.com>
2022-01-08 09:43:56 -0300

    add solution to first exercise of lesson 4

---
+++ 1.js
@@ -0,0 +1,18 @@
+const readlineSync = require('readline-sync');
+let run = true;
+let gradesSum = 0;
+let gradesTotal = 0;
+
+while (run){
+	const grade = readlineSync.question('insira a nota: ');
+
+	if (grade === 'fim'){
+		run = false;
+	} else {
+		gradesSum += Number(grade);
+		gradesTotal += 1;
+	}
+}
+const media = gradesSum / gradesTotal
+
+console.log(`A média das notas digitadas é ${media}`);
