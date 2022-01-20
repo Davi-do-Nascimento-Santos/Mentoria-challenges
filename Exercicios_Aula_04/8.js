a026e90e50504210c27735a2da49e894f904efa1
Davi Nascimento <davinascimento19072001@gmail.com>
2022-01-20 20:43:38 -0300

    solution question 8 lesson 4

---
+++ Exercicios_Aula_04/8.js
@@ -0,0 +1,20 @@
+console.log('Coffee Shop');
+console.log('   Menu');
+console.log('1 - Coca Cola - R$ 4.00');
+console.log('2 - Coxinha   - R$ 2.50');
+console.log('3 - Pastel    - R$ 3.00');
+console.log('4 - Suco      - R$ 3.50');
+const readlineSync = require('readline-sync');
+let fim = true;
+let total = 0;
+const menu = [4, 2.50, 3, 3.50]
+while (fim){
+    let item = readlineSync.question('Item: ');
+    if (item == 'fim'){
+        fim = false;
+    } else {
+        let quantidade = Number(readlineSync.question('Quantidade: '));
+        total += menu[Number(item) - 1] * quantidade;
+    }
+}
+console.log(`Valor total: R$ ${total}`);
