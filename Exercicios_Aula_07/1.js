6ad12b75dec526110aa78f1c0ddd516eddc312cc
Davi Nascimento <davinascimento19072001@gmail.com>
2022-01-28 21:29:36 -0300

    solutions of exercises 1 and 2 of lesson 7

---
+++ Exercicios_Aula_07/1.js
@@ -0,0 +1,3 @@
+const data = Date().split(' ');
+const data_full = {'Day': data[2], 'Month': data[1], 'Year': data[3]};
+console.log(data_full);
---
+++ Exercicios_Aula_07/2.js
@@ -0,0 +1,2 @@
+const date = {'Day': 1, 'Month': 'janeiro', 'Year': 2017};
+console.log(`${date["Day"]} de ${date["Month"]} de ${date["Year"]}`);
