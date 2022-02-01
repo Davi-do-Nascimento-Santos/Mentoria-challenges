44329ad34ff230ee6d3922f9eca0f9f2e50b2c48
Davi Nascimento <davinascimento19072001@gmail.com>
2022-02-01 10:21:31 -0300

    solution of exercise 3 of lesson 11

---
+++ Exercicios_Aula_11/3.js
@@ -0,0 +1,15 @@
+// 3 - Crie uma função que recebe um array e  retorna o último elemento
+// desse array se nenhum outro argumento for passado, porém se outro
+// argumento for passado ela vai retornar os últimos "n" elementod do
+// array.
+function first (array, n=0){
+    if (Array.isArray(array) === true){
+        if (n === 0){
+            return array[array.length-1];
+        } else{
+            return array.slice(n-1);
+        }
+    }
+}
+console.log(first([1, 2, 3]));
+console.log(first([1, 2, 3], 2)); 
