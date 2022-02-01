5f1792b761ce3aca2530c0f11404c37ea149267b
Davi Nascimento <davinascimento19072001@gmail.com>
2022-02-01 10:47:12 -0300

    solution of exercise 7 of lesson 11

---
+++ Exercicios_Aula_11/7.js
@@ -0,0 +1,14 @@
+// 7 - Escreva uma funçãoq para encontrar um elemento em um array
+// caso exista retornar a posição, caso não existir retornar null.
+function encontra(array, n){
+    if (Array.isArray(array) === true){
+        for (let i=0; i < array.length; i++){
+            if (array[i] === n){
+                return i;
+            }
+        }
+        return null;
+    }
+}
+console.log(encontra([1, 2, 3, 4, 5], 4));
+
