e46e8ace76c90e38ac2cfc6c6cd5988fdf2cb1e6
Davi Nascimento <davinascimento19072001@gmail.com>
2022-02-01 20:34:50 -0300

    solution of exercise 8 of lesson 11

---
+++ Exercicios_Aula_11/8.js
@@ -0,0 +1,21 @@
+// 8 - Escreva uma função para gerar um array entre dois inteiros.
+function rangeBetween(a, b){
+    const array = [];
+    if (a < b){
+        for (let i = a; i <= b; i++){
+            array.push(i);
+        }
+        return array;
+    } else if (a > b){
+        for (let i = a; i >= b; i--){
+            array.push(i);
+        }
+        return array;
+    }
+
+}
+console.log(rangeBetween(-10, 10));
+console.log(rangeBetween(-10, 10));
+console.log(rangeBetween(4, 7));
+console.log(rangeBetween(-4, -7));
+console.log(rangeBetween(-7, -4));
