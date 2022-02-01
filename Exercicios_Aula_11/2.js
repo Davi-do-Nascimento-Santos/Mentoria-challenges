6471f6359c9cebde0dfe793483c29fd42d31e5a8
Davi Nascimento <davinascimento19072001@gmail.com>
2022-02-01 10:08:58 -0300

    solution of exercise 2 of lesson 11

---
+++ Exercicios_Aula_11/2.js
@@ -0,0 +1,12 @@
+// 2 - Escreva uma função que retorna o primeiro elemento de um array.
+function first (array, n=1){
+    if (Array.isArray(array) === true){
+        if (n === 1){
+            return array[0];
+        } else {
+            return array.slice(0, n);
+        }
+    }
+}
+console.log(first([1, 2, 3, 4, 5]));
+console.log(first([10, 9, 1, 2, 3, 4], 5));
