de2d5a205d011cfcc6bab6ee0628622dc2677099
Davi Nascimento <davinascimento19072001@gmail.com>
2022-01-31 22:21:15 -0300

    solution of exercise 2 of lesson 7

--- Exercicios_Aula_07/2.js
+++ Exercicios_Aula_07/2.js
@@ -1,2 +1,3 @@
+// 2 - crie um objeto de data com a data de 1 de Janeiro de 2017.
 const date = {'Day': 1, 'Month': 'janeiro', 'Year': 2017};
 console.log(`${date["Day"]} de ${date["Month"]} de ${date["Year"]}`);
